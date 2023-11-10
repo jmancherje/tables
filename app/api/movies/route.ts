7b02c7d9f6e97d82ffc0dfe6087b9e5ed83a8119
Justin Mancherje <jmancherje@nextdoor.com>
2023-11-10 00:09:00 -0800

    Try removing try catch

--- app/api/movies/route.ts
+++ app/api/movies/route.ts
@@ -4,20 +4,15 @@ import path from "path";
 export async function GET() {
   const data = await fs.readFile("./data/moviesByHref.json");
 
-  try {
-    const moviesByHref = JSON.parse(data.toString());
-    const hrefs = Object.keys(moviesByHref);
+  const moviesByHref = JSON.parse(data.toString());
+  const hrefs = Object.keys(moviesByHref);
 
-    const names = hrefs.map((href) => {
-      const movie = moviesByHref[href];
-      return [movie.name, href];
-    });
+  const names = hrefs.map((href) => {
+    const movie = moviesByHref[href];
+    return [movie.name, href];
+  });
 
-    fs.writeFile("./data/sortedMovieNames.json", JSON.stringify(names));
+  fs.writeFile("./data/sortedMovieNames.json", JSON.stringify(names));
 
-    return Response.json({ data: hrefs.length });
-  } catch (error) {
-    console.error("Error parsing people.json", error);
-    return Response.error();
-  }
+  return Response.json({ data: hrefs.length });
 }
